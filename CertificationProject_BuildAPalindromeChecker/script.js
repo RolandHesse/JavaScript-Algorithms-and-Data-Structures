3b0e871d131b9ddd9a913195b75a592ef096e0b6
Roland Hesse <rolandhesse11783@gmail.com>
2024-01-24 12:44:56 +0100

    start first certification project

---
+++ CertificationProject_BuildAPalindromeChecker/script.js
@@ -0,0 +1,8 @@
+const inputForm = document.getElementById("form");
+const result = document.getElementById("result");
+
+inputForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const userTextInput = event.target.elements.textInput.value;
+ alert("Please input a value")
+})
