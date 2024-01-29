3529468366c2b26a6f202c3125792500b009efc2
Roland Hesse <rolandhesse11783@gmail.com>
2024-01-29 19:48:58 +0100

    add some style

--- CertificationProject_BuildAPalindromeChecker/script.js
+++ CertificationProject_BuildAPalindromeChecker/script.js
@@ -1,8 +1,33 @@
 const inputForm = document.getElementById("form");
-const result = document.getElementById("result");
+const resultDiv = document.getElementById("result");
 
 inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const userTextInput = event.target.elements.textInput.value;
- alert("Please input a value")
-})
+
+  if (userTextInput === "") {
+    alert("Please input a value");
+    return;
+  }
+
+  const lowercasedUserTextInput = userTextInput.toLowerCase();
+  const regex = /[^a-z\d]/g;
+  const clearedUserTextInput = lowercasedUserTextInput.replace(regex, "");
+  const reversedClearedUserTextInput = clearedUserTextInput
+    .split("")
+    .reverse()
+    .join("");
+
+  if (clearedUserTextInput === reversedClearedUserTextInput) {
+    resultDiv.innerHTML = `<strong>${userTextInput}</strong> is a palindrome`;
+    event.target.reset();
+  } else {
+    resultDiv.innerHTML = `<strong>${userTextInput}</strong> is not a palindrome`;
+    event.target.reset();
+  }
+
+  console.log("userTextInput: ", userTextInput);
+  console.log("lowercasedUserTextInput: ", lowercasedUserTextInput);
+  console.log("clearedUserTextInput: ", clearedUserTextInput);
+  console.log("reversedClearedUserTextInput: ", reversedClearedUserTextInput);
+});
