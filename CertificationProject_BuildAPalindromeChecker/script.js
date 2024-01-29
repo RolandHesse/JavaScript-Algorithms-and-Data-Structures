0fde68c1fcc9ba6f93d6b245dc1375d0d60edc9a
Roland Hesse <rolandhesse11783@gmail.com>
2024-01-29 20:10:15 +0100

    add confetti

--- CertificationProject_BuildAPalindromeChecker/script.js
+++ CertificationProject_BuildAPalindromeChecker/script.js
@@ -1,6 +1,35 @@
 const inputForm = document.getElementById("form");
 const resultDiv = document.getElementById("result");
 
+function confetti() {
+  function random(max) {
+    return Math.random() * max;
+  }
+
+  const documentFragment = document.createDocumentFragment();
+
+  for (let i = 0; i < 100; i++) {
+    let styles =
+      "transform: translate3d(" +
+      (random(500) - 250) +
+      "px, " +
+      (random(200) - 150) +
+      "px, 0) rotate(" +
+      random(360) +
+      "deg);\
+                background: hsla(" +
+      random(360) +
+      ",100%,50%,1);\
+                animation: bang 700ms ease-out forwards;\
+                opacity: 0";
+
+    const randomlyStyledConfetti = document.createElement("i");
+    randomlyStyledConfetti.style.cssText = styles.toString();
+    documentFragment.appendChild(randomlyStyledConfetti);
+  }
+  inputForm.append(documentFragment);
+}
+
 inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const userTextInput = event.target.elements.textInput.value;
@@ -20,14 +49,10 @@ inputForm.addEventListener("submit", (event) => {
 
   if (clearedUserTextInput === reversedClearedUserTextInput) {
     resultDiv.innerHTML = `<strong>${userTextInput}</strong> is a palindrome`;
+    confetti();
     event.target.reset();
   } else {
     resultDiv.innerHTML = `<strong>${userTextInput}</strong> is not a palindrome`;
     event.target.reset();
   }
-
-  console.log("userTextInput: ", userTextInput);
-  console.log("lowercasedUserTextInput: ", lowercasedUserTextInput);
-  console.log("clearedUserTextInput: ", clearedUserTextInput);
-  console.log("reversedClearedUserTextInput: ", reversedClearedUserTextInput);
 });
