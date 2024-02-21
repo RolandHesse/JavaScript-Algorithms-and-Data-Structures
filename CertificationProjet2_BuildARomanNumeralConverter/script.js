c3a7bea2f04b3a6d73689362494b79e2d3eba115
Roland Hesse <rolandhesse11783@gmail.com>
2024-02-21 16:14:54 +0100

    refactor and clean up code (no styles yet)

--- CertificationProjet2_BuildARomanNumeralConverter/script.js
+++ CertificationProjet2_BuildARomanNumeralConverter/script.js
@@ -2,8 +2,54 @@ const input = document.getElementById("number");
 const convertButton = document.getElementById("convert-btn");
 const output = document.getElementById("output");
 
-convertButton.addEventListener("click", () => {
-  const outputArray = [];
+// let residuum;
+// let outputArray = [];
+
+const transform1051 = (
+  highArabic,
+  middleArabic,
+  lowArabic,
+  highRoman,
+  middleRoman,
+  lowRoman
+) => {
+  residuum = input.value % highArabic;
+
+  const numberOfMiddleArabicsInResiduum = Math.floor(residuum / middleArabic);
+  residuum = residuum % middleArabic;
+  const numberOfLowArabicsInResiduumOfResiduum = Math.floor(
+    residuum / lowArabic
+  );
+
+  if (numberOfMiddleArabicsInResiduum === 1) {
+    if (numberOfLowArabicsInResiduumOfResiduum === 0) {
+      outputArray.push(middleRoman);
+    } else if (numberOfLowArabicsInResiduumOfResiduum < 4) {
+      outputArray.push(middleRoman);
+      for (let i = 0; i < numberOfLowArabicsInResiduumOfResiduum; i++) {
+        outputArray.push(lowRoman);
+      }
+    } else if (numberOfLowArabicsInResiduumOfResiduum === 4) {
+      outputArray.push(lowRoman + highRoman);
+    }
+  }
+
+  if (numberOfMiddleArabicsInResiduum === 0) {
+    if (numberOfLowArabicsInResiduumOfResiduum < 4) {
+      for (let i = 0; i < numberOfLowArabicsInResiduumOfResiduum; i++) {
+        outputArray.push(lowRoman);
+      }
+    }
+    if (numberOfLowArabicsInResiduumOfResiduum === 4) {
+      outputArray.push(lowRoman + middleRoman);
+    }
+  }
+};
+
+convertButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  outputArray = [];
   output.textContent = "";
 
   {
@@ -20,107 +66,16 @@ convertButton.addEventListener("click", () => {
       return;
     }
 
-    let residuum;
-
     const thousands = Math.floor(input.value / 1000);
     for (let i = 0; i < thousands; i++) {
       outputArray.push("M");
     }
-    residuum = input.value % 1000;
 
-    const fivehundreds = Math.floor(residuum / 500);
-    residuum = residuum % 500;
-    const hundreds = Math.floor(residuum / 100);
-
-    if (fivehundreds === 1) {
-      if (hundreds === 0) {
-        outputArray.push("D");
-      } else if (hundreds < 4) {
-        outputArray.push("D");
-        for (let i = 0; i < hundreds; i++) {
-          outputArray.push("C");
-        }
-      } else if (hundreds === 4) {
-        outputArray.push("CM");
-      }
-    }
-
-    if (fivehundreds === 0) {
-      if (hundreds < 4) {
-        for (let i = 0; i < hundreds; i++) {
-          outputArray.push("C");
-        }
-      }
-      if (hundreds === 4) {
-        outputArray.push("CD");
-      }
-    }
-
-    residuum = residuum % 100;
-
-    const fifties = Math.floor(residuum / 50);
-    residuum = residuum % 50;
-    const tens = Math.floor(residuum / 10);
-
-    if (fifties === 1) {
-      if (tens === 0) {
-        outputArray.push("L");
-      } else if (tens < 4) {
-        outputArray.push("L");
-        for (let i = 0; i < tens; i++) {
-          outputArray.push("X");
-        }
-      } else if (tens === 4) {
-        outputArray.push("XC");
-      }
-    }
-
-    if (fifties === 0) {
-      if (tens < 4) {
-        for (let i = 0; i < tens; i++) {
-          outputArray.push("X");
-        }
-      }
-      if (tens === 4) {
-        outputArray.push("XL");
-      }
-    }
-
-    residuum = residuum % 10;
-
-    const fives = Math.floor(residuum / 5);
-    residuum = residuum % 5;
-    const ones = Math.floor(residuum / 1);
-
-    if (fives === 1) {
-      if (ones === 0) {
-        outputArray.push("V");
-      } else if (ones < 4) {
-        outputArray.push("V");
-        for (let i = 0; i < ones; i++) {
-          outputArray.push("I");
-        }
-      } else if (ones === 4) {
-        outputArray.push("IX");
-      }
-    }
-
-    if (fives === 0) {
-      if (ones < 4) {
-        for (let i = 0; i < ones; i++) {
-          outputArray.push("I");
-        }
-      }
-      if (ones === 4) {
-        outputArray.push("IV");
-      }
-    }
+    transform1051(1000, 500, 100, "M", "D", "C");
+    transform1051(100, 50, 10, "C", "L", "X");
+    transform1051(10, 5, 1, "X", "V", "I");
 
     const outputString = outputArray.join("");
-    output.innerText = outputString;
-
-    console.log("outputArray: ", outputArray);
-    console.log("outputString: ", outputString);
-    console.log("residuum: ", residuum);
+    output.textContent = outputString;
   }
 });
